6aec81a2b8a36319167df83999495506ce606bf1
Eduardo Ferreira <efbomfilho@yahoo.com>
2026-08-30 10:51:42 -0300

    feat: add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription("API documentatio for the authentication service")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
